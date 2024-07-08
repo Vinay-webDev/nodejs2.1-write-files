1bd711aa14bc38e277b30e069123cebb20adffd2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-08 22:34:55 +0530

    1. how to import when there is huge chunck of data

---
+++ stream.js
@@ -0,0 +1,32 @@
+// when we have huge data files
+const fs = require('fs');
+const path = require('path');
+
+const rs = fs.createReadStream(path.join(__dirname, 'files', 'lorem.txt'), {encoding: 'utf8'});
+
+const ws = fs.createWriteStream(path.join(__dirname, 'files', 'newlorem.txt'));
+
+// now we'll listen for data!!!
+rs.on('data', (dataChunck) => {
+    ws.write(dataChunck);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
