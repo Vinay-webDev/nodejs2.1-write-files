59f4ad683c618d52c88ec4992fa9301b28140344
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-08 23:01:22 +0530

    4. check to see if there is directory that already exists using fs.existsSync( ) method****
    fs.existsSync( ) & fs.rmdir( )

--- dir.js
+++ dir.js
@@ -1,15 +1,27 @@
 // how to make directory and how to delete directory in node*****
 
 const fs = require('fs');
-
+/*
 fs.mkdir('./new', (err) => {
     if (err) throw err;
     console.log("Directory created!");
-});
+}); */
 
+// to check to see if there a directory that already exists using existsSync() method
+if (!fs.existsSync('./new')) {
+    fs.mkdir('./new', (err) => {
+        if(err) throw err;
+        console.log("Directory created!");
+    })
+};
 
-
-
+// to delete the existing directory 
+if (fs.existsSync('./new')) {
+    fs.rmdir('./new', (err) => {
+        if (err) throw err;
+        console.log("Directory removed!");
+    })
+};
 
 
 
